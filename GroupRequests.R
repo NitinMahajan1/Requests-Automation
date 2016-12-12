db1d1d6f8face68bae1683f98f7e08c6ff8f2969
NitinMahajan1 <nitin_mahajan@icloud.com>
2016-12-12 09:14:58 -0500

    Requests_Group_V0.1

--- GroupRequests.R
+++ GroupRequests.R
@@ -7,21 +7,38 @@ library(dplyr)
 library(openxlsx)
 library(zoo)
 
-data_file<-read.xlsx('Consolidated.xlsx')
-
 # data_file<-data_file[c(14,13,1,9)]
 ##data_file<-data_file[c(14,13,1:12)]
 
+data_file<-read.xlsx('Consolidated.xlsx')
 data_df<-tbl_df(data_file)
+data_df<-data_df[c(2,14,13,1,9)]
 
 data_df$Closed_At<- as.Date(data_df$Closed_At)
 data_df$Closed_At<- as.yearmon(data_df$Closed_At,format="%b %Y")
 
-data_df<-data_df[c(2,14,13,1,9)]
-  
-  
 by_Ops<- group_by(data_df,Ops_Grp,Ops_Team,Source,Closed_At)
 
 agg<-summarise(by_Ops,count=n_distinct(Number))
+
+
+
+data_awf<-read.xlsx('awf historical volumes.xlsx')
+awf_df<-tbl_df(data_awf)
+awf_df$Date<-convertToDateTime(awf_df$Date, origin = "1900-01-01")
+awf_df$Date<-as.yearmon(awf_df$Date,format="%b %Y")
+
+colnames(awf_df)[which(names(awf_df) == "Ops.Team")] <- "Ops_Team"
+colnames(awf_df)[which(names(awf_df) == "Ops.Grp")] <- "Ops_Grp"
+colnames(awf_df)[which(names(awf_df) == "Distinct.count.of.Number")] <- "count"
+colnames(awf_df)[which(names(awf_df) == "Date")] <- "Closed_At"
+
+awf_dataframe<-as.data.frame(awf_df)
+agg_dataframe<-as.data.frame(agg_df)
+
+new<-rbind(agg_dataframe,awf_dataframe)
+
+
+new<-rbind(agg,awf_df)??????????????????????
 
 write.xlsx(agg, file="Consolidated_Agg.xlsx",row.names=FALSE)
